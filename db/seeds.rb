9abe9505d30a1229fd9a5dfb9fc42f54dc38e574
Danielle <s3621257@student.rmit.edu.au>
2019-05-23 09:11:12 +0000

    Tute 9 production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+
+User.create!(name:  "Danielle Xu",
+             email: "danielle.xu@rmit.edu.au",
+             password:              "password1",
+             password_confirmation: "password1",
+             admin: true)
+             
+User.create!(name:  "Jarene Xu",
+             email: "jarene.xu@rmit.edu.au",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+             
+User.create!(name:  "Laeticia Bucktowar",
+             email: "lbuckto@rmit.edu.au",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)             
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
