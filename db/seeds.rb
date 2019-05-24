eac7a99722e8c71481cf6413a4bd15aff7008abd
Danielle <s3621257@student.rmit.edu.au>
2019-05-24 03:52:08 +0000

    deploying to heroku for email test

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,33 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 User.create!(name:  "Danielle Xu",
              email: "danielle.xu@rmit.edu.au",
              password:              "password1",
              password_confirmation: "password1",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)             
              
 User.create!(name:  "Jarene Xu",
              email: "jarene.xu@rmit.edu.au",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)   
              
 User.create!(name:  "Laeticia Bucktowar",
              email: "lbuckto@rmit.edu.au",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)             
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)             
 
 99.times do |n|
   name  = Faker::Name.name
@@ -29,5 +37,7 @@ User.create!(name:  "Laeticia Bucktowar",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
